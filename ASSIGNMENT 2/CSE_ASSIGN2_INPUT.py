2cf51c603e7b96231fb0628aca67ba1c54df17d0
Jyoti Dixit <97622078+JyotiDixit172@users.noreply.github.com>
2022-01-26 18:02:57 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/CSE_ASSIGN2_INPUT.py
@@ -0,0 +1,72 @@
+# Question 1
+str1="Python is a case sensitive language" 
+print("<A>length og string is:",len(str1)) #function to find LENGTH OF STRING
+print("<B>Reversed String is:",str1[::-1])
+str2=str1[10:26] #STORED a case sensitive IN A NEW STRING
+str2.replace("a case sensitive","object oriented") #REPLACED "a case sensitive" WITH "object oriented"
+print("<E>The index of substring a is ",str1.find('a'))
+print("<F>The original string after removing whitespaces is",str1.replace(" ",""))
+print("\n")
+
+
+#Question 2
+Name=input("ENTER YOUR NAME:")
+SID=int(input("ENTER YOUR SID:"))
+Department=input("ENTER YOUR DEPARTMENT:")
+CGPA=float(input("ENTER YOUR CGPA:"))
+print("Hey %s,"%Name,"Here!")
+print("My SID is %d" %SID)
+print("I am from %s"%Department,"and my CGPA is %f"%CGPA)
+print("\n")
+
+#Question 3
+a=60
+b=20
+print("a. ",a&b)
+print("b. ",a|b)
+print("c. ",a^b)
+print("Left shift both a and b with 2 bits respectively are :",a<<2 ,b<<2)
+print("Right shift a with 2 bits and b with 4 bits respectively are : ",a>>2,b>>4)
+print("\n")
+
+
+#Question 4
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+ 
+if (num1 >=num2) and (num1 >=num3):
+   largest = num1
+elif (num2 >=num1) and (num2 >=num3):
+   largest = num2
+else:
+   largest = num3
+ 
+print("THE LARGEST NUMBER IS ",largest)
+print("\n")
+
+
+
+#Question 5
+s=input("ENTER A STRING:")
+if 'name' in s:
+    print ("YES")
+else:
+    print ("NO")
+print("\n")
+
+
+#Question 6
+a=float(input("ENTER FIRST SIDE OF TRIANGLE:"))
+b=float(input("ENTER SECOND SIDE OF TRIANGLE:"))
+c=float(input("ENTER THIRD SIDE OF TRIANGLE:"))
+if(a>=(b+c)):      #Equality sign is used as if sum of two sides is equal to third then also triangle is not valid
+    print("No")
+elif(b>=(a+c)):
+    print("No")
+elif(c>=(a+b)):
+    print("No")
+else:
+    print("Yes")    
+
+
---
+++ ASSIGNMENT 2/CSE_ASSIGN2_OUTPUT.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:/Users/Asus/OneDrive/Desktop/CSE_ASSIGN2_INPUT.py =========
+<A>length og string is: 35
+<B>Reversed String is: egaugnal evitisnes esac a si nohtyP
+<E>The index of substring a is  10
+<F>The original string after removing whitespaces is Pythonisacasesensitivelanguage
+
+
+ENTER YOUR NAME:Jyoti Dixit
+ENTER YOUR SID:21103013
+ENTER YOUR DEPARTMENT:CSE
+ENTER YOUR CGPA:9.5
+Hey Jyoti Dixit, Here!
+My SID is 21103013
+I am from CSE and my CGPA is 9.500000
+
+
+a.  20
+b.  60
+c.  40
+Left shift both a and b with 2 bits respectively are : 240 80
+Right shift a with 2 bits and b with 4 bits respectively are :  15 1
+
+
+Enter first number: 11
+Enter second number: 12
+Enter third number: 13
+THE LARGEST NUMBER IS  13.0
+
+
+ENTER A STRING:Jyoti
+NO
+
+
+ENTER FIRST SIDE OF TRIANGLE:25
+ENTER SECOND SIDE OF TRIANGLE:26
+ENTER THIRD SIDE OF TRIANGLE:27
+Yes
