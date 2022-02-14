604637dad0140665129b7231738588b416b7fcfe
Jyoti Dixit <97622078+JyotiDixit172@users.noreply.github.com>
2022-02-14 22:31:21 +0530

    Update assign3_cse_21103013_source_code.py

--- ASSIGN_3/assign3_cse_21103013_source_code.py
+++ ASSIGN_3/assign3_cse_21103013_source_code.py
@@ -199,23 +199,22 @@ print("\n")
 
 #Answer_7
 print("Answer_7")
-#  Function to display the Fibonacci sequence
-def recur_fibo(n):
+def recur_fibo(n):            #  Function to display the Fibonacci sequence
    if n <= 1:
        return n
    else:
        return(recur_fibo(n-1) + recur_fibo(n-2))
-no_of_terms=int(input("ENTER THE NUMBER OF TERMS IN THE SERIES: "))
-if no_of_terms <= 0:     # Check if the number of terms is valid
+no_of_terms=int(input("ENTER NUMBER OF TERMS IN REQUIRED IN FIBONACCI SERIES: "))
+if no_of_terms <= 0:               # Check if the number of terms is valid
    print("Plese enter a positive integer")
 else:
-   print("Fibonacci sequence:")
+   print("The Fibonacci sequence is:")
    sum=0
    for i in range(no_of_terms):
        print(recur_fibo(i))
        sum=sum+recur_fibo(i)
-avg=float(sum/no_of_terms)
-print("AVERAGE OF RESULTANT FIBONACCI SERIES IS",avg)
+avg=float(sum/no_of_terms)          # CALCULATING THE AVERAGE THE OF FIBONACCI SEQUENCE
+print("AVERAGE OF FIBONACCI SERIES IS:",avg)
 
 
 #Answer_8
